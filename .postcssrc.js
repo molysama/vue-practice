641aac159d8baebdfcbca819f4ad4927ff894ac5
molysama <molysama.i@gmail.com>
2017-09-19 11:42:04 +0800

    添加插件
    Signed-off-by: molysama <molysama.i@gmail.com>

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,6 +4,9 @@ module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {},
+
+    // 将px转换成vw、vh
     "postcss-px-to-viewport": {}
   }
 }
+
---
+++ src/views/index.vue
@@ -0,0 +1,14 @@
+
+<template lang="pug">
+
+h1 hello,world
+  
+</template>
+<style lang="stylus" scoped>
+
+h1 
+    font-size 18px
+
+</style>
+
+
