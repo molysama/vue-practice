5c94ac191a0f6cac1b74e5121f1f88422dd19ab1
molysama <molysama.i@gmail.com>
2017-09-24 17:19:35 +0800

    添加遮罩层组件masker

---
+++ src/components/mask.vue
@@ -0,0 +1,49 @@
+
+<template lang="pug" >
+
+.masker(v-show="showMasker", @click.stop="close")
+    slot
+  
+</template>
+<script>
+export default {
+    name: 'masker',
+    data () {
+        return {
+            showMasker: false
+        }
+    },
+    watch: {
+        show (v, oldV) {
+            this.showMasker = v
+        }
+    },
+    props: {
+        show: {
+            default: false,
+            type: Boolean
+        }
+    },
+    methods: {
+        open () {
+            this.showMasker = true
+        },
+        close () {
+            this.showMasker = false
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.masker
+    position absolute 
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background rgba(0, 0, 0, .3)
+    z-index 2
+
+</style>
+
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,29 @@
 <template lang="pug">
 
-.page
+.page(@click="open")
+    masker(ref="masker", :show="parentshow")
     page-header
-    section.container
+    section.container CONTENT
 </template>
 <script>
 
+export default {
+    data () {
+        return {
+            parentshow: true
+        }
+    },
+    methods: {
+        open () {
+            this.$refs['masker'].open()
+        }
+    }
+}
+
 </script>
 <style lang="stylus">
+.page 
+    width 100%
 .container
     height 1500px
 </style>
